6c6c764c735d0a631bb8e9818d9974c8e16e44b1
greenmapc <greenmapc@gmail.com>
2020-11-18 22:14:01 +0300

    some fix

--- lesson8/svm.py
+++ lesson8/svm.py
@@ -44,16 +44,19 @@ while play:
                 clf = svm.SVC(kernel='linear', C=1.0)
                 clf.fit(points, clusters)
 
-                # получение веса  w из (w * x - b = 0)
-                w = clf.coef_[0]
-                # преобразование формулы на a * x = 0
-                a = -w[0] / w[1]
+                # получение веса  w и b из (w * x - b = 0)
+                weights = clf.coef_[0]
+                w = weights[0]
+                b = weights[1]
 
-                xx = np.linspace(0, 600, 2)
+                # преобразование формулы на a * x = 0
+                a = -w / b
+
+                xx = np.linspace(0, max_x, 2)
 
                 # подставляем значения в решающую функцию, чтобы получить ее значение
                 # где clf.intercept_[0] - коэффициенты в уравнении разделяющей гиперплоскости
-                yy = a * xx - (clf.intercept_[0]) / w[1]
+                yy = a * xx - (clf.intercept_[0]) / b
 
                 pygame.draw.line(display, (0, 0, 0),
                                  (xx[0], yy[0]),
